3d68cd6914020cd4ac7ef5ade61cce67908f6d7c
ThoR <61441120+thomasrolff@users.noreply.github.com>
2020-04-09 17:40:24 +0200

    debugged mobile navigation menu, it now displays properly. Todo: transition on mobile

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,19 @@
+const toggler = document.querySelector("input[name=checkbox]");
+const menu = document.querySelector(".menu-mobile");
+const menuOverlay = document.querySelector(".menu-overlay");
+
+toggler.addEventListener("change", (e) => {
+    // toggle menu visibility 
+    if(e.target.checked) {
+        menu.style.visibility = "visible";
+        menuOverlay.style.transform = "translateX(0%)";
+        menuOverlay.style.transition = "transform .15s ease";
+        document.body.setAttribute("class", "noscroll");
+
+    } else {
+        menu.style.visibility = "hidden";
+        menuOverlay.style.transform = "translateX(100%)";
+        menuOverlay.style.transition = "transform .15s ease";
+        document.body.removeAttribute("class", "noscroll");
+    }
+});
