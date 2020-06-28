08af749feedea3148f7ac34393569cd4178b3ae2
ThoR <61441120+thomasrolff@users.noreply.github.com>
2020-06-28 18:01:57 +0200

    Add state to Album component with React Hooks
     - Import useState from react
     - Add state to Album function component
     - Toggle overlay on click
     - @Todo: create Overlay component based on props

--- src/js/components/Albums.js
+++ src/js/components/Albums.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import goSurfing from '../../img/album-go-surfing.jpg';
 import v from '../../img/album-v.jpg';
 import superjazz from '../../img/album-superjazz.jpg';
 import madpack from '../../img/album-madpack.jpg';
 import fire from '../../img/album-fire.jpg';
 import bruut from '../../img/album-bruut.jpg';
+// import ToggleOverlay from './ToggleOverlay';
+
 
 function Album(props) {
+  const [overlayOn, toggleOverlay] = useState(false);
+
   return (
     <div className="album">
       <img src={props.image}></img>
-      <div className="overlay">
+      <div className="overlay" onClick={() => toggleOverlay(!overlayOn)}>
+        
+        {overlayOn && (
+          <div className="overlay-wrapper">
+           {props.album.released}
+            {/* @ TODO: Overlay Component 
+                <Overlay album={props.album} />
+            */}
+
+          </div>
+        )}
+
         <h3 className="albumTitle">{props.album.title}</h3>
         <p className="album-info"></p>
       </div>
@@ -18,10 +33,11 @@ function Album(props) {
   );
 }
 
+
 export default function Albums(props) {
   return (
     <div className="albums-container">
-      <Album album={props.albums.goSurfing} image={goSurfing}/>
+      <Album album={props.albums.goSurfing} image={goSurfing} />
       <Album album={props.albums.v} image={v}/>
       <Album album={props.albums.superjazz} image={superjazz}/>
       <Album album={props.albums.madpack} image={madpack}/>
