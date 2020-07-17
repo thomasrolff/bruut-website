261a449817670cf50a474ce0b19990a764a7d371
ThoR <61441120+thomasrolff@users.noreply.github.com>
2020-07-18 00:10:16 +0200

    Add autoprefixer
     - Install postscc-loader, autoprefixer
     - Add postcss.config.js
     - Add browserslist to package.json
     - @Todo: finetune browserslist

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,7 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader, // 3. inject styles into DOM (for prod: MiniCssExtractPlugin.loader)
           "css-loader", // 2. turns css into common js
+          "postcss-loader",
           "sass-loader", // 1. turns sass into css
         ],
       },
